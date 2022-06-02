155ef3b86cff484f40cc0529cb46211e8dc692d0
CrazyCatZhang <CrazyCatZhang@outlook.com>
2022-06-02 23:58:09 +0800

    Implement headers and add functions

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import MyHeader from './components/Header'
+import AddInput from "./components/AddInput";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isInputShow, setInputShow] = useState(false),
+        [todoList, setTodoList] = useState([]);
+
+    const addItem = (value) => {
+        // console.log(value);
+        const data = {
+            id: Date.now().toString(),
+            content: value,
+            completed: false
+        }
+        setTodoList(todoList => [...todoList,data])
+        setInputShow(false)
+    }
+    return (
+        <div className="App">
+            <MyHeader openInput={() => setInputShow(!isInputShow)}/>
+            <AddInput isInputShow={isInputShow} addItem={addItem}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ todolist/src/components/TodoItem/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TodoItem(props) {
+    return (
+        <li>
+
+        </li>
+    );
+}
+
+export default TodoItem;
